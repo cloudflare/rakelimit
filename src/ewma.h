2d7e20affdf8b5fed8f8de1e27bb2c196253ca3c
Jonas Otten <jonas@cloudflare.com>
2020-09-17 14:27:46 +0100

    cleanup ewma & musl headers

--- src/ewma.h
+++ src/ewma.h
@@ -5,7 +5,9 @@
 #include "common.h"
 #include "fixed-point.h"
 
-#define WINDOW 1000000000ull
+// 100ms
+#define WINDOW_NS 100000000ull
+#define ONE_SECOND_NS 1000000000ull
 
 // estimate_avg_rate takes a previous rate and a duration that elapsed
 // since this rate has been determined, and estimates based on these and
@@ -18,19 +20,19 @@ static fpoint FORCE_INLINE estimate_avg_rate(fpoint old_rate, __s64 dur)
 	}
 
 	// calculate pps since last timestamp
-	__u64 rate_current = 1000000000ull / dur;
+	__u64 rate_current = to_fixed_point(ONE_SECOND_NS / dur);
 	// if the last timestamp is older than the window the new pps will simply be rate_current
-	if (dur >= WINDOW) {
-		return to_fixed_point(rate_current);
+	if (dur >= WINDOW_NS) {
+		return rate_current;
 	}
 
-	fpoint a = to_fixed_point(dur) / WINDOW;
+	fpoint a = to_fixed_point(dur) / WINDOW_NS;
 
 	fpoint new_rate = old_rate;
-	if (old_rate > to_fixed_point(rate_current)) {
-		new_rate -= a * to_int(old_rate - to_fixed_point(rate_current));
+	if (old_rate > rate_current) {
+		new_rate -= a * to_int(old_rate - rate_current);
 	} else {
-		new_rate += a * to_int(to_fixed_point(rate_current) - old_rate);
+		new_rate += a * to_int(rate_current - old_rate);
 	}
 	return new_rate;
 }
