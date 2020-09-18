4389222650df4b26aa2a8f5b7ee130980cc006e7
Lorenz Bauer <lmb@cloudflare.com>
2020-09-18 09:43:30 +0100

    bpf: do not use packed attribute unless necessary
    Using a packed representation seems to make the compiler emit stores
    to the packed struct in single bytes. Remove the attribute if we don't
    need it.

--- src/packet_element.h
+++ src/packet_element.h
@@ -8,6 +8,6 @@ struct packet_element {
 	__u16 destination_port;
 	__u8 source_address[IP_BUFFER_LENGTH];
 	__u8 destination_address[IP_BUFFER_LENGTH];
-} __attribute__((packed));
+};
 
 _Static_assert(sizeof(struct packet_element) == IP_BUFFER_LENGTH + IP_BUFFER_LENGTH + 4, "wrong packet_element size");
